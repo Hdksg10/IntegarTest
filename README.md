b596856de4d32e5c88a19c79fb9f6fed538824ef
HdKsg10 <1427207878@qq.com>
2022-04-08 17:49:59 +0800

    change readme

--- README.md
+++ README.md
@@ -8,8 +8,8 @@
 
 - 测试任意函数的运行时间
 - 生成指定范围的随机整数
-- 生成指定长度范围的随机C风格Int类型数组，随机Int类型vector
-- 测试接受整数/C风格Int类型数组，Int类型vector作为参数的函数在不同输入规模下的运行时间
+- 生成指定长度范围的随机~~C风格int类型数组~~，随机int类型vector
+- 测试接受整数/~~C风格int类型数组~~，int类型vector作为参数的函数在不同输入规模下的运行时间
 
 ## 快速使用教程
 
@@ -62,3 +62,69 @@ test2.67 test2.67 test2.67 test2.67 test2.67 test2.67 test2.67 test2.67 test2.67
 ```
 
 ## 详细介绍（施工中）
+
+### includes
+
+- <random>
+- <chrono>
+- <iosteam>
+- <vector>
+
+### classes
+
+#### IntegarTest
+
+Constructions
+
+```C++
+IntegarTest()//默认构造函数，设置随机数范围为[1, 10000)，默认随机测试类型为有限个整数
+IntegarTest(int lo, int high)//构造函数，设置随机数范围为[lo, high)，默认随机测试类型为有限个整数
+IntegarTest(int lo, int high, int type)//构造函数，设置随机数范围为[lo, high)，随机测试类型为type对应（见成员变量_type的介绍)
+```
+
+Desructions
+
+```c++
+ ~IntegarTest()//析构函数，主要处理数组内存的释放
+```
+
+Member Variables
+
+```c++
+std::vector<double> _timeCost//存放多次随机调用所测的时间
+int32_t _lo//随机数生成器的最小值
+Int32_t _hi//随机数生成器的最大值（不含)
+Int8_t _type//内置随机测试函数的测试类型1 = 有限个整数， 2 = int类型vector， 3 = int类型C风格数组，functest函数不受限制
+std::vector<int> _testVector//存储用于测试的vector
+Int* _testArray//用于测试的C风格数组
+Int32_t _testIntegar//用于测试的随机整数
+std::uniform_int_distribution<> _distib//用于生成随机整数的标准均匀分布
+std::mt19937 _randomEngine//用于生成随机数的随机数生成器
+```
+
+Member Functions
+
+```c++
+Private:
+int _randomIntegar(int lo, int high)// 返回[lo, hi)范围内的随机整数
+void _initVector(size_t size, std::vector<int>& target)// 对Target vector随机初始化，随机初始化后大小为size
+void _initArray(size_t size, int* target)//对大小为size的C风格数组target随机初始化
+template<class FuncT, typename ...args>void _functionCall(FuncT Func, args...arg)//用于调用任意函数，接受一个可调用对象作为第一个参数，以及可变个其他参数，可变个其他参数作为第一个参数可调用对象Func的参数
+Public：
+//随机数生成
+int getRandomIntegar(int lo, int high)//返回[lo, high)范围内的随机整数
+//随机测试
+template<class FunctorT>double singleTest(FunctorT& Func)//对只接受一个整数作为参数的仿函数Func进行单次随机调用测试，返回运行消耗时间（单位：毫秒）
+template<class FunctorT>double singleTest(FunctorT& Func, size_t size)//对只接受一个整数vector作为参数的仿函数Func进行单次随机调用测试，返回运行消耗时间（单位：毫秒）
+template<class FunctorT>void multiTest(FunctorT& Func, int frequency)//对只接受一个整数作为参数的仿函数Func进行多次随机调用测试，frequency为测试次数， 返回累计消耗时间（单位：毫秒）
+template<class FunctorT>void multiTest(FunctorT& Func, int minSize, int maxSize, int step)//对只接受一个整数vector作为参数的仿函数Func进行多次随机调用测试，数据规模从minSize增长到maxSize(不含)，每次增加step，每次调用消耗时间存储在_timeCost中
+//时间测试
+template<class FuncT, typename ...args>double functest(FuncT Func, args ... arg)//用于对任意函数进行单次运行时间测试，接受一个可调用对象作为第一个参数，以及可变个其他参数，接受的可变个其他参数作为第一个参数(即可调用对象Func)的参数，返回运行时间（单位：毫秒），不受_type指定类型的限制
+```
+
+Friend Function
+
+```c++
+friend std::ostream& operator<<(std::ostream& os , const IntegarTest &It)//重载的<<运算符，将IntegerTest中_timeCost中的元素全部输出到os这个标准输出流中
+```
+
--- include/IntegarTest.hpp
+++ include/IntegarTest.hpp
@@ -14,40 +14,40 @@ class IntegarTest
     friend std::ostream& operator<<(std::ostream& os , const IntegarTest &It);
 private:
     std::vector<double> _timeCost;
-    int32_t __lo;
-    int32_t __hi;
+    int32_t _lo;
+    int32_t _hi;
     std::mt19937 _randomEngine;
-    int8_t __type;//1 = 有限个数字， 2 = int类型vector， 3 = int类型C风格数组
-    int32_t __testIntegar;
-    std::vector<int> __testVector;
-    std::uniform_int_distribution<> __distib;
-    std::vector<int> __randomIntegarSets;
-    int* __testArray;
-    int __randomIntegar(int lo, int high)
+    int8_t _type;//1 = 有限个数字， 2 = int类型vector， 3 = int类型C风格数组
+    int32_t _testIntegar;
+    std::vector<int> _testVector;
+    std::uniform_int_distribution<> _distib;
+    std::vector<int> _randomIntegarSets;
+    int* _testArray;
+    int _randomIntegar(int lo, int high)
     {
-         return __distib(_randomEngine);
+         return _distib(_randomEngine);
     }
-    void __initVector(size_t size, std::vector<int>& target)
+    void _initVector(size_t size, std::vector<int>& target)
     {
         target.clear();
         for(int i = 0; i < size; i++)
         {
-            target.push_back(__randomIntegar(__lo, __hi));
+            target.push_back(_randomIntegar(_lo, _hi));
         }
     }
-    void __initArray(size_t size, int* target)
+    void _initArray(size_t size, int* target)
     {
         if(target)
             delete[] target;
         target = new int[size];
         for(int i = 0; i < size; i++)
         {
-            target[i] = __randomIntegar(__lo, __hi);
+            target[i] = _randomIntegar(_lo, _hi);
         }
 
     }
     template<class FuncT, typename ...args>
-    void __functorCall(FuncT Func, args ... arg)
+    void _functorCall(FuncT Func, args ... arg)
     {
         Func(arg...);
     }         
@@ -55,52 +55,52 @@ public:
     IntegarTest()
     {
         _randomEngine.seed(time(nullptr));
-        __testArray = nullptr;
-        __lo = 1;
-        __hi = 10000;
-        __type = 1;
-        std::uniform_int_distribution<>::param_type _params{__lo, __hi};
-        __distib.param(_params);
+        _testArray = nullptr;
+        _lo = 1;
+        _hi = 10000;
+        _type = 1;
+        std::uniform_int_distribution<>::param_type _params{_lo, _hi};
+        _distib.param(_params);
     }
 
     IntegarTest(int lo, int high)
     {
         _randomEngine.seed(time(nullptr));
-        __testArray = nullptr;
-        __lo = lo;
-        __hi = high;
-        __type = 1;
-        std::uniform_int_distribution<>::param_type _params{__lo, __hi};
-        __distib.param(_params);
+        _testArray = nullptr;
+        _lo = lo;
+        _hi = high;
+        _type = 1;
+        std::uniform_int_distribution<>::param_type _params{_lo, _hi};
+        _distib.param(_params);
     }
 
     IntegarTest(int lo, int high, int type)
     {
         _randomEngine.seed(time(nullptr));
-        __testArray = nullptr;
-        __lo = lo;
-        __hi = high;
-        __type = type;
-        std::uniform_int_distribution<>::param_type _params{__lo, __hi};
-        __distib.param(_params);
+        _testArray = nullptr;
+        _lo = lo;
+        _hi = high;
+        _type = type;
+        std::uniform_int_distribution<>::param_type _params{_lo, _hi};
+        _distib.param(_params);
     }
     ~IntegarTest()
     {
-        if(__testArray)
-            delete[] __testArray;
+        if(_testArray)
+            delete[] _testArray;
     }
     int getRandomIntegar(int lo, int high)
     {
-        return __randomIntegar(lo, high);
+        return _randomIntegar(lo, high);
     }
     template<class FunctorT>
     double singleTest(FunctorT& Func)
     {
-        if(__type == 1)
+        if(_type == 1)
         {
-            __testIntegar = __randomIntegar(__lo, __hi);
+            _testIntegar = _randomIntegar(_lo, _hi);
             std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
-            __functorCall(Func, __testIntegar);
+            _functorCall(Func, _testIntegar);
             std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
             std::chrono::duration<double, std::milli> time_span = std::chrono::duration_cast<std::chrono::duration<double, std::milli>>(end - begin);
             return time_span.count();
@@ -113,10 +113,10 @@ public:
     template<class FunctorT>
     double singleTest(FunctorT& Func, size_t size)
     {
-        if(__type == 2)
+        if(_type == 2)
         {
-            __initVector(size, __testVector);
-            return functest(Func, __testVector);
+            _initVector(size, _testVector);
+            return functest(Func, _testVector);
         }
         // else if(__type == 3)
         // {
@@ -129,31 +129,32 @@ public:
         }
     }
     template<class FunctorT>
-    void multiTest(FunctorT& Func, int frequency)
+    double multiTest(FunctorT& Func, int frequency)
     {
         for(int i = 0; i < frequency; i++)
         {
-            __randomIntegarSets.push_back(__randomIntegar(__lo, __hi));
+            _randomIntegarSets.push_back(_randomIntegar(_lo, _hi));
         }
         std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
         for(int i = 0; i< frequency; i++)
         {
-            __functorCall(Func, __randomIntegarSets[i]);
+            _functorCall(Func, _randomIntegarSets[i]);
         }
         std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
         std::chrono::duration<double, std::milli> time_span = std::chrono::duration_cast<std::chrono::duration<double, std::milli>>(end - begin);
-        __randomIntegarSets.clear();
+        _randomIntegarSets.clear();
+        return time_span.count();
     }
     template<class FunctorT>
     void multiTest(FunctorT& Func, int minSize, int maxSize, int step)
     {
-        if(__type == 2)
+        if(_type == 2)
         {
             for(int i = minSize; i < maxSize; i = i + step)
             {
-                __initVector(i, __testVector);
+                _initVector(i, _testVector);
                 std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
-                __functorCall(Func, __testVector);
+                _functorCall(Func, _testVector);
                 std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
                 std::chrono::duration<double, std::milli> time_span = std::chrono::duration_cast<std::chrono::duration<double, std::milli>>(end - begin);
                 _timeCost.push_back(time_span.count());
@@ -171,7 +172,7 @@ public:
     double functest(FuncT Func, args ... arg)
     {
         std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
-        __functorCall(Func, arg...);
+        _functorCall(Func, arg...);
         std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
         std::chrono::duration<double, std::milli> time_span = std::chrono::duration_cast<std::chrono::duration<double, std::milli>>(end - begin);
         return time_span.count();
